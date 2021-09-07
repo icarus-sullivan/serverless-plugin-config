5916414181c70e28a751e6b299633863b62eadc1
Chris Sullivan <chrissullivan.dev@gmail.com>
2021-09-07 00:05:31 -0700

    Fixing resolver issues

--- src/index.js
+++ src/index.js
@@ -5,21 +5,27 @@ const IDENTIFIER = 'config:';
 const IDENTIFIER_COLOR = '\x1b[32m';
 const COLOR_RESET = '\x1b[0m';
 const DEFAULT_DIRECTORY = 'config';
+
+const SELF_RESOLVE_IDENTIFIER = 'self';
 class Config {
   constructor(sls, options) {
     this.serverless = sls;
 
     const dir = sls?.custom?.configs?.dir || DEFAULT_DIRECTORY;
+    const currentConfig = this.serverless?.service?.custom || {};
     process.env.NODE_CONFIG_DIR = join(process.cwd(), dir);
     process.env.NODE_CONFIG_ENV = options?.stage || sls?.service?.provider?.stage;
 
-    const config = {...require('config')};
-    sls.service.custom = {...sls.service.custom, ...config};
-    this.config = config;
+    this.config = require('config');
+    this.serverless.service.custom = Object.assign(currentConfig, this.config);
+
+    const selfResolveIndex = sls.variables.variableResolvers.findIndex((it) => it.regex.toString().includes(SELF_RESOLVE_IDENTIFIER));
+
+    this.defaultSelfResolver = sls.variables.variableResolvers[selfResolveIndex].resolver;
+    this.serverless.variables.variableResolvers[selfResolveIndex].resolver = this.resolveConfig.bind(this);
 
     this.hooks = {
       [`${PLUGIN}:debug:debug`]: this.printDebug.bind(this),
-      [`before:package:initialize`]: this.log.bind(this, `Building configuration for stage ${process.env.NODE_CONFIG_ENV}`),
     };
 
     this.commands = {
@@ -34,8 +40,18 @@ class Config {
         },
       },
     };
+  }
 
-    
+  async resolveConfig(...args) {
+    console.log('args', ...args);
+    const [self_key] = args;
+
+    if (self_key.includes('config')) {
+      const key = self_key.replace('self:config.', '');
+      return this.config.get(key) || null;
+    }
+
+    return await this.defaultSelfResolver(...args);
   }
 
   log(...args) {
@@ -45,7 +61,8 @@ class Config {
   }
 
   printDebug() {
-    this.log(JSON.stringify(this.config, null, 2));
+    const custom = this.serverless?.service?.custom || {};
+    this.log(JSON.stringify(custom, null, 2));
   }
 }
 
